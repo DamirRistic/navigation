976017c4cf9b29cfdf708e4b9a1f8d760686234a
Damir Ristic <damirristic1987@gmail.com>
2020-10-04 06:16:35 +0200

    Updates

---
+++ precache-manifest.2e529e494139bafe2a84e9aa8025f6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a851a440d20d9a0bc2fa94f445db63",
+    "url": "/navigation/index.html"
+  },
+  {
+    "revision": "07e8f40ae05ad0a0f065",
+    "url": "/navigation/static/js/2.7a588210.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/navigation/static/js/2.7a588210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226f686e682ed1671a46",
+    "url": "/navigation/static/js/main.6eef5aa4.chunk.js"
+  },
+  {
+    "revision": "20b10aa853b8e70aabdf",
+    "url": "/navigation/static/js/runtime-main.509824a9.js"
+  },
+  {
+    "revision": "2090551770be22b09600a40b0b4673b7",
+    "url": "/navigation/static/media/Avenir-Medium.20905517.ttf"
+  }
+]);
